92862daa16bcfe1d3674d1ab8bd61c14d9ad8aeb
Sjors Lockhorst <sjorslockhorst@protonmail.com>
2023-03-21 17:31:51 +0100

    First commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/tailwindcss'
+  ],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  }
+})
+
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-gray-700">this is some text</h1>
+  </div>
+</template>
